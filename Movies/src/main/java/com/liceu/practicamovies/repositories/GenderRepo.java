2565e1bf35734d83f5ec08d3f6db56aa5a0ae0c1
qseopqsm <drakognome@gmail.com>
2022-02-03 22:02:57 +0100

    again

--- Movies/src/main/java/com/liceu/practicamovies/repositories/GenderRepo.java
+++
@@ -1,7 +0,0 @@
-package com.liceu.practicamovies.repositories;
-
-import com.liceu.practicamovies.models.Gender;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GenderRepo extends JpaRepository<Gender,Integer> {
-}
